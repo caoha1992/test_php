d73a864a7d66a2b0128288718af8ab35b0ee5d38
caoha1992 <141201204+caoha1992@users.noreply.github.com>
2023-08-04 16:38:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+use Core\Router;
+use Core\Session;
+
+const BASE_PATH = __DIR__.'/../';
+require BASE_PATH.'/Core/functions.php';
+//spl_autoload_register(callback: function ($class){
+//    $class = str_replace("\\","/", $class);
+//    require base_path("{$class}.php");
+//});
+require BASE_PATH.'/vendor/autoload.php';
+require base_path('bootstrap.php');
+$router =  new Router();
+require base_path('routes.php');
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_SERVER['REQUEST_METHOD'] ?? $_POST['_method'];
+$router->router($uri,$method);
+Session::unflash('error');
+Session::unflash('old');
+
+
